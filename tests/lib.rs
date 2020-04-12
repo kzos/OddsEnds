e380509f3c2043ee55c7f2a51ca0cad95878febf
kzos <zaheersheriff.k@gmail.com>
2020-04-12 16:25:33 +0530

    Necessary project files to directly clone and work on it

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,6 +2,9 @@
 mod tests {
     //Test cases are good in rust
     extern crate OddsEnds;
+    #[test]
+    #[should_panic]
+    #[ignore]
     fn english_greeting_correct()
     {
         assert_eq!("hellods", OddsEnds::greetings::english::hello());
